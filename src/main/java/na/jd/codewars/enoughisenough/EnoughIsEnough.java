8a35ad736be93ebe3e8732a93bac7640074da940
gpinolo <g.decesare@gmail.com>
2019-02-09 20:34:44 +0100

    Enough Is Enough

--- src/main/java/na/jd/codewars/enoughisenough/EnoughIsEnough.java
+++ src/main/java/na/jd/codewars/enoughisenough/EnoughIsEnough.java
@@ -1,7 +1,28 @@
 package na.jd.codewars.enoughisenough;
 
+import java.util.*;
+
 class EnoughIsEnough {
-    static int[] deleteNth(int[] list, int occurence){
-        return null;
+    static int[] deleteNth(int[] input, int occurence) {
+        Map<Integer, Integer> howManyAdd = new HashMap<>();
+        List<Integer> outputList = new ArrayList<>();
+        Arrays.stream(input).forEach(element -> {
+            Integer count = howManyAdd.get(element);
+            if (count == null || count < occurence) {
+                outputList.add(element);
+                howManyAdd.put(element, count != null ? count + 1 : 1);
+            }
+        });
+        return convert(outputList);
+    }
+
+    private static int[] convert(List<Integer> outputList) {
+        int size = outputList.size();
+        int[] result = new int[size];
+        Integer[] temp = outputList.toArray(new Integer[size]);
+        for (int n = 0; n < size; ++n) {
+            result[n] = temp[n];
+        }
+        return result;
     }
 }
--- src/test/java/na/jd/codewars/enoughisenough/EnoughIsEnoughTest.java
+++ src/test/java/na/jd/codewars/enoughisenough/EnoughIsEnoughTest.java
@@ -2,13 +2,16 @@ package na.jd.codewars.enoughisenough;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EnoughIsEnoughTest {
     @Test
     public void deleteNth() throws Exception {
-        assertEquals(new int[]{20,37,21}, EnoughIsEnough.deleteNth(new int[] {20,37,20,21}, 1));
-        assertEquals(new int[]{1, 1, 3, 3, 7, 2, 2, 2}, EnoughIsEnough.deleteNth(new int[] {1,1,3,3,7,2,2,2,2}, 3));
+        assertTrue(Arrays.compare(new int[]{20, 37, 21}, EnoughIsEnough.deleteNth(new int[]{20, 37, 20, 21}, 1)) == 0);
+        assertTrue(Arrays.compare(new int[]{1, 1, 3, 3, 7, 2, 2, 2}, EnoughIsEnough.deleteNth(new int[]{1, 1, 3, 3, 7, 2, 2, 2, 2}, 3)) == 0);
     }
 
 }
