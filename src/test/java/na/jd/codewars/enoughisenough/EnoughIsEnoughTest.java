0f04e9958249c0c2ab7135e60055091895b882fd
gdecesare <g.decesare@emminformatica.it>
2019-02-11 13:35:28 +0100

    quality

--- src/test/java/na/jd/codewars/enoughisenough/EnoughIsEnoughTest.java
+++ src/test/java/na/jd/codewars/enoughisenough/EnoughIsEnoughTest.java
@@ -4,12 +4,11 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class EnoughIsEnoughTest {
     @Test
-    public void deleteNth() throws Exception {
+    public void deleteNth() {
         assertTrue(Arrays.compare(new int[]{20, 37, 21}, EnoughIsEnough.deleteNth(new int[]{20, 37, 20, 21}, 1)) == 0);
         assertTrue(Arrays.compare(new int[]{1, 1, 3, 3, 7, 2, 2, 2}, EnoughIsEnough.deleteNth(new int[]{1, 1, 3, 3, 7, 2, 2, 2, 2}, 3)) == 0);
     }
