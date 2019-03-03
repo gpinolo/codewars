7d05d55e976d140915ba9f0a8b8ef125c9f35530
gpinolo <g.decesare@gmail.com>
2019-03-03 17:44:08 +0100

    repetita iuvant

--- src/main/java/na/jd/codewars/humadreadabletime/HumanReadableTimeRepeat1.java
+++ src/main/java/na/jd/codewars/humadreadabletime/HumanReadableTimeRepeat1.java
@@ -2,9 +2,6 @@ package na.jd.codewars.humadreadabletime;
 
 public class HumanReadableTimeRepeat1 {
     public static String makeReadable(int i) {
-        int hour = i/3600;
-        int minute = (i - hour * 3600)/60;
-        int second = i - hour * 3600 - minute*60;
-        return String.format("%02d:%02d:%02d", hour, minute, second);
+        return String.format("%02d:%02d:%02d", i/3600, i/60%60, i % 60);
     }
 }
